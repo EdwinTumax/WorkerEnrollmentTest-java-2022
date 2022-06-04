d1319640ee4a3b82692ff7c7be276d4570ac69e6
Edwin Tumax <edwintumax@gmail.com>
2022-06-04 13:17:11 -0600

    Finalizando Worker para crear inscripciones

--- src/main/java/edu/kalum/workerenrollment/core/WorkerEnrollmentApplication.java
+++ src/main/java/edu/kalum/workerenrollment/core/WorkerEnrollmentApplication.java
@@ -1,9 +1,11 @@
 package edu.kalum.workerenrollment.core;
 
+import edu.kalum.workerenrollment.core.verticles.EnrollmentProcessVerticle;
 import edu.kalum.workerenrollment.core.verticles.QueueConsummerVerticle;
 import io.vertx.config.ConfigRetriever;
 import io.vertx.config.ConfigRetrieverOptions;
 import io.vertx.config.ConfigStoreOptions;
+import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Vertx;
 import io.vertx.core.json.JsonObject;
 import org.slf4j.Logger;
@@ -13,30 +15,25 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
-
 import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class WorkerEnrollmentApplication implements CommandLineRunner {
-
     private static final Logger logger = LoggerFactory.getLogger(WorkerEnrollmentApplication.class);
     private Vertx vertx;
-
     @Autowired
     private Environment env;
-
     @Autowired
     private QueueConsummerVerticle queueConsummerVerticle;
-
+    @Autowired
+    private EnrollmentProcessVerticle enrollmentProcessVerticle;
     public static void main(String[] args) {
         SpringApplication.run(WorkerEnrollmentApplication.class, args);
     }
-
     @Override
     public void run(String... args) throws Exception {
         logger.debug("Iniciando app consummer rabbit");
     }
-
     @PostConstruct
     public void deployVerticle(){
         this.vertx = Vertx.vertx();
@@ -46,7 +43,9 @@ public class WorkerEnrollmentApplication implements CommandLineRunner {
         ConfigRetriever configRetriever = ConfigRetriever.create(vertx,configRetrieverOptions);
         configRetriever.getConfig(config -> {
             if(config.succeeded()){
-                logger.debug("Conexion exitosa!!");
+                this.vertx.deployVerticle(queueConsummerVerticle, new DeploymentOptions().setConfig(config.result()));
+                this.vertx.deployVerticle(enrollmentProcessVerticle, new DeploymentOptions().setConfig(config.result()));
+                logger.info("Se realizo el deployment de los verticles exitosamente!!");
             } else {
                 logger.error("Error al desplegar los verticles ".concat(config.cause().getMessage()));
             }
